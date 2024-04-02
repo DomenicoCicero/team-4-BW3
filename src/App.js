6719b5d7c0f5662b8a9adf8fa8ceee4c7f102467
Marco Pala <155745769+marcopa94@users.noreply.github.com>
2024-04-02 17:31:27 +0100

    commitfinalwiithoutconflict

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import SidebarDx from "./components/SidebarDx.jsx";
-import "./App.css";
+import "./style.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 function App() {
   return <SidebarDx></SidebarDx>;
