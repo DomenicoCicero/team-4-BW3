7cda7eac694fcf5c5756a0f386d66650016ab220
Marco Pala <155745769+marcopa94@users.noreply.github.com>
2024-04-03 11:43:10 +0100

    experience add

--- src/components/EsperienzaProfilo.jsx
+++ src/components/EsperienzaProfilo.jsx
@@ -6,10 +6,10 @@ import { FaPen } from "react-icons/fa";
 import { FaPlus } from "react-icons/fa";
 import { useSelector } from "react-redux";
 import { Dropdown } from "react-bootstrap";
-import { BsFillBriefcaseFill } from "react-icons/bs";
+import { BsBraces, BsFillBriefcaseFill } from "react-icons/bs";
 import { IoCalendarNumber } from "react-icons/io5";
 const EsperienzaProfilo = () => {
-  const user = useSelector(state => {
+  const user = useSelector((state) => {
     return state.profilo.user;
   });
   return (
@@ -38,8 +38,41 @@ const EsperienzaProfilo = () => {
           </div>
         </Col>
       </Row>
-      <h5>{user.title}</h5>
-      <p>{user.bio}</p>
+      {/*       -----------------------------------------inizio part dinamica---------------------------------- */}
+      <Row>
+        <Col xs={1}>
+          <img src="./img/7.jpeg" alt="" style={{ width: "70px" }}></img>{" "}
+        </Col>
+        <Col xs={11} style={{ paddingLeft: "30px" }}>
+          {/*             <div className="textexperience"> */}
+          <h3>Prova2</h3>
+          <p className="mb-0">esperienza0</p>
+          <span>esperienza 1</span>
+          <br></br>
+          <span>esperienza 1</span>
+
+          <h5 className="pt-3">prova2</h5>
+          <hr></hr>
+        </Col>
+      </Row>
+      <Row>
+        <Col xs={1}>
+          <img src="./img/7.jpeg" alt="" style={{ width: "70px" }}></img>{" "}
+        </Col>
+        <Col xs={11} style={{ paddingLeft: "30px" }}>
+          {/*             <div className="textexperience"> */}
+          <h3>Prova2</h3>
+          <p className="mb-0">esperienza0</p>
+          <span>esperienza 1</span>
+          <br></br>
+          <span>esperienza 1</span>
+
+          <h5 className="pt-3">prova2</h5>
+          <hr></hr>
+        </Col>
+      </Row>
+
+      {/*       -----------------------------------------fine part dinamica---------------------------------- */}
     </Container>
   );
 };
