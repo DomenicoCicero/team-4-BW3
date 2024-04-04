b5ff3e4b8d7206056bb496a4fcf8b7ecf48ef8b7
DomenicoCicero <domenico.cicero8@gmail.com>
2024-04-04 10:54:58 +0200

    finishes put-endpoint

--- src/components/CardExp.jsx
+++ src/components/CardExp.jsx
@@ -4,15 +4,15 @@ const CardExp = props => {
   return (
     <Container fluid>
       <Row>
-        <Col xs={1}>
-          <img src={props.exp.image} alt="" style={{ width: "70px" }}></img>{" "}
+        <Col xs={2}>
+          <img src={props.exp.image} alt="" className="w-100"></img>{" "}
         </Col>
         <Col xs={8} style={{ paddingLeft: "30px" }}>
           <h3>{props.exp.role}</h3>
           <p className="mb-0">{props.exp.company}</p>
           <span>{props.exp.area}</span>
           <br></br>
-          <span>{props.exp.startDate}</span>
+          <span>{props.exp.startDate.substring(0, 10)}</span>
           <span>{props.exp.endDate}</span>
 
           <p className="pt-3">{props.exp.description}</p>
